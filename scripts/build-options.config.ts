f9f02275c80c75220e61ad072b6f0741d21162e3
ScarletFlash <scarletflash.dev@gmail.com>
2022-03-21 01:54:11 +0300

    build: setup optimized build

--- scripts/build-options.config.ts
+++ scripts/build-options.config.ts
@@ -3,11 +3,14 @@ import { sassPlugin, SassPluginOptions } from 'esbuild-sass-plugin';
 import { join } from 'path';
 
 const scriptDirectoryPath: string = __dirname;
-const sourcesDirPath: string = join(scriptDirectoryPath, './../src');
-const resultBundleDirPath: string = join(scriptDirectoryPath, './../dist');
+const rootDirPath: string = join(scriptDirectoryPath, './../');
+const sourcesDirPath: string = join(rootDirPath, './src');
+const resultBundleDirPath: string = join(rootDirPath, './dist');
+const tsConfigPath: string = join(rootDirPath, './tsconfig.app.json');
 
 const sassPluginOptions: SassPluginOptions = {
-  type: 'css-text'
+  type: 'css-text',
+  transform: (css: string) => css.replaceAll('\n', '').replaceAll(' ', '')
 };
 
 export const buildOptions: BuildOptions = {
@@ -17,5 +20,12 @@ export const buildOptions: BuildOptions = {
   resolveExtensions: ['.ts', '.js', '.scss'],
   plugins: [sassPlugin(sassPluginOptions)],
   platform: 'browser',
-  format: 'iife'
+  format: 'esm',
+  charset: 'utf8',
+  minify: true,
+  target: 'esnext',
+  treeShaking: true,
+  tsconfig: tsConfigPath,
+  sourcemap: true,
+  legalComments: 'external'
 };
