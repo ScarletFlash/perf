084dd00954c51622f9a8de6a05a0d1a5936263d7
ScarletFlash <scarletflash.dev@gmail.com>
2022-03-20 18:01:16 +0300

    build: configure ESBuild to handle SCSS

--- scripts/serve.ts
+++ scripts/serve.ts
@@ -1,5 +1,6 @@
 import { log } from 'console';
 import { BuildOptions, serve, ServeOptions, ServeResult } from 'esbuild';
+import { sassPlugin, SassPluginOptions } from 'esbuild-sass-plugin';
 import { join } from 'path';
 
 const scriptDirectoryPath: string = __dirname;
@@ -9,10 +10,15 @@ const serveOptions: ServeOptions = {
   servedir: sourcesDirPath
 };
 
+const sassPluginOptions: SassPluginOptions = {
+  type: 'css'
+};
+
 const buildOptions: BuildOptions = {
   bundle: true,
   outdir: sourcesDirPath,
-  entryPoints: [`${sourcesDirPath}/index.ts`]
+  entryPoints: [`${sourcesDirPath}/index.ts`, `${sourcesDirPath}/index.scss`],
+  plugins: [sassPlugin(sassPluginOptions)]
 };
 
 serve(serveOptions, buildOptions).then(({ host, port }: ServeResult) => {
