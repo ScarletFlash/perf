04fadd0e79463114b8c164a13c0635b2d4b81727
ScarletFlash <scarletflash.dev@gmail.com>
2022-05-06 18:07:50 +0300

    refactor: move current route handling into routing.service

--- src/background-services/routing.service.ts
+++ src/background-services/routing.service.ts
@@ -1,6 +1,17 @@
+import { Application } from '@application';
 import { Route } from '@declarations/interfaces/route.interface';
+import { OnHashChangeCallback } from '@declarations/types/on-hash-change-callback.type';
+import { OnRouteChangeCallback } from '@declarations/types/on-route-change-callback.type';
+import { TitleService } from './title.service';
+import { UrlService } from './url.service';
 
 export class RoutingService {
+  readonly #urlService: UrlService = Application.getBackgroundService(UrlService);
+  readonly #titleService: TitleService = Application.getBackgroundService(TitleService);
+
+  readonly #onRouteChangeCallbacks: Set<OnRouteChangeCallback> = new Set<OnRouteChangeCallback>();
+  #currentRoute: Route | undefined;
+
   static readonly #routes: Route[] = [
     {
       componentSelector: 'perf-route-code',
@@ -29,7 +40,32 @@ export class RoutingService {
     return RoutingService.#routes;
   }
 
+  constructor() {
+    const onHashChange: OnHashChangeCallback = (currentHash: string) => {
+      const foundRoute: Route | undefined = this.getRouteByUrlHash(currentHash);
+      const targetRoute: Route = foundRoute === undefined ? RoutingService.#routes.at(0) : foundRoute;
+      if (this.#currentRoute === targetRoute) {
+        return;
+      }
+
+      this.#currentRoute = targetRoute;
+      this.#titleService.setTitle(targetRoute.title);
+      this.#onRouteChangeCallbacks.forEach((callback: OnRouteChangeCallback) => callback(this.#currentRoute));
+    };
+
+    console.log({ url: this.#urlService });
+    this.#urlService.subscribeToHashChanges(onHashChange);
+  }
+
   public getRouteByUrlHash(hash: string): Route | undefined {
     return RoutingService.#routes.find(({ urlHash }: Route) => urlHash.toLowerCase() === hash.toLowerCase());
+  }
+
+  public subscribeToRouteChanges(callback: OnRouteChangeCallback): void {
+    this.#onRouteChangeCallbacks.add(callback);
+  }
+
+  public unsubscribeFromRouteChanges(callback: OnRouteChangeCallback): void {
+    this.#onRouteChangeCallbacks.delete(callback);
   }
 }
