3ddc60cbfccebd71285525dddb9714ed9ea61056
ScarletFlash <scarletflash.dev@gmail.com>
2022-05-06 23:21:35 +0300

    chore: add editor config

--- src/components/routes/route-code/editor.ts
+++ src/components/routes/route-code/editor.ts
@@ -4,6 +4,54 @@ export class Editor {
   static #monacoEditorEnvironmentSettingsAreDefined: boolean = false;
   #monacoEditor: editor.IStandaloneCodeEditor | null = null;
 
+  readonly #monacoEditorOptions: editor.IStandaloneEditorConstructionOptions = {
+    value: `
+/** @perf_Common Iterate via Array.prototype.forEach */
+const dataSet: number[] = new Array(100_000).fill(null).map(() => Math.random());
+
+let result: unknown[];
+
+/** @perf_BeforeEach Iterate via Array.prototype.forEach */
+result = [];
+
+/** @perf_Test Iterate via Array.prototype.forEach */
+dataSet.forEach((item: number) => result.push(item));
+
+/** @perf_Test Iterate via for...of */
+for (const item of dataSet) {
+  result.push(item);
+}
+
+/** @perf_Test Iterate via for */
+for (let index = 0; index < dataSet.length; index++) {
+  result.push(dataSet[index]);
+}
+    `,
+    language: 'typescript',
+    autoDetectHighContrast: false,
+    theme: 'vs-dark',
+    minimap: {
+      enabled: false
+    },
+    scrollbar: {
+      horizontal: 'hidden'
+    },
+    contextmenu: false,
+    largeFileOptimizations: false,
+    padding: {
+      top: 0,
+      bottom: 0
+    },
+    emptySelectionClipboard: true,
+    columnSelection: true,
+    cursorSmoothCaretAnimation: true,
+    cursorBlinking: 'smooth',
+    renderLineHighlightOnlyWhenFocus: true,
+    disableMonospaceOptimizations: true,
+    tabSize: 2,
+    insertSpaces: true
+  };
+
   readonly #containerElement: HTMLElement;
 
   constructor(container: HTMLElement) {
@@ -16,10 +64,7 @@ export class Editor {
       throw new Error('Editor is already running');
     }
 
-    this.#monacoEditor = editor.create(this.#containerElement, {
-      value: "function hello() {\n\talert('Hello world!');\n}",
-      language: 'javascript'
-    });
+    this.#monacoEditor = editor.create(this.#containerElement, this.#monacoEditorOptions);
   }
 
   public destroy(): void {
