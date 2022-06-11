2966acae03ca9a56ba9eb4337789945547552cda
ScarletFlash <scarletflash.dev@gmail.com>
2022-06-11 17:19:07 +0300

    refactor: make isUrlHash private method of UrlService

--- src/application/utilities/is-url-hash.util.ts
+++
@@ -1,5 +0,0 @@
-import type { UrlHash } from '@application/declarations/types/url-hash.type';
-
-export function isUrlHash(input: string): input is UrlHash {
-  return input === '' || input.startsWith('#');
-}
